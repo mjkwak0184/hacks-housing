b4f4f2206110d4aa60b91a60f9c9de9f122fbeef
Terry Kim <terrykim@Terrys-MacBook-Pro.local>
2023-10-28 15:33:26 -0700

    added styles

--- app/app/app.py
+++ app/app/app.py
@@ -9,4 +9,5 @@ import reflex as rx
 
 # Create the app and compile it.
 app = rx.App(style=styles.base_style)
+
 app.compile()
--- app/app/styles.py
+++ app/app/styles.py
@@ -1,63 +1,78 @@
-"""Styles for the app."""
-
 import reflex as rx
 
-border_radius = "0.375rem"
-box_shadow = "0px 0px 0px 1px rgba(84, 82, 95, 0.14)"
-border = "1px solid #F4F3F6"
-text_color = "black"
-accent_text_color = "#1A1060"
-accent_color = "#F5EFFE"
-hover_accent_color = {"_hover": {"color": accent_color}}
+# Basic Style Variables
+border_radius = "0.5rem"
+box_shadow = "0px 4px 10px rgba(0, 0, 0, 0.1)"
+border = "1px solid #E0E0E0"
+text_color = "#333"
+accent_text_color = "#0055AA"
+accent_color = "#EAF2FF"
+hover_accent_color = {"_hover": {"color": accent_text_color}}
 hover_accent_bg = {"_hover": {"bg": accent_color}}
-content_width_vw = "90vw"
-sidebar_width = "20em"
+content_width_vw = "85vw"
+sidebar_width = "22em"
 
-template_page_style = {"padding_top": "1em", "padding_x": ["auto", "2em"]}
+# Page & Content Styles
+template_page_style = {
+    "padding_top": "2em",
+    "padding_x": ["auto", "3em"]
+}
 
 template_content_style = {
     "width": "100%",
     "align_items": "flex-start",
     "box_shadow": box_shadow,
     "border_radius": border_radius,
-    "padding": "1em",
-    "margin_bottom": "1em",
+    "padding": "1.5em",
+    "margin_bottom": "2em",
+    "background": "#FFFFFF"
 }
 
+# Link Styles
 link_style = {
-    "color": text_color,
+    "color": accent_text_color,
     "text_decoration": "none",
     **hover_accent_color,
 }
 
+# Button Styles
 overlapping_button_style = {
-    "background_color": "white",
+    "background_color": "#FFFFFF",
     "border": border,
     "border_radius": border_radius,
+    "transition": "all 0.3s ease",
+    "_hover": {
+        "background_color": accent_color,
+        "box_shadow": "0px 4px 15px rgba(0, 0, 0, 0.1)"
+    }
 }
 
+# Base Styles
 base_style = {
     rx.MenuButton: {
-        "width": "3em",
-        "height": "3em",
+        "width": "3.5em",
+        "height": "3.5em",
         **overlapping_button_style,
     },
     rx.MenuItem: hover_accent_bg,
 }
 
+# Markdown Styles
 markdown_style = {
-    "code": lambda text: rx.code(text, color="#1F1944", bg="#EAE4FD"),
+    "code": lambda text: rx.code(text, color="#333", bg="#F5F5F5"),
     "a": lambda text, **props: rx.link(
         text,
         **props,
         font_weight="bold",
-        color="#03030B",
+        color=accent_text_color,
         text_decoration="underline",
-        text_decoration_color="#AD9BF8",
+        text_decoration_color=accent_color,
         _hover={
-            "color": "#AD9BF8",
-            "text_decoration": "underline",
-            "text_decoration_color": "#03030B",
+            "color": "#FFFFFF",
+            "background_color": accent_text_color,
+            "text_decoration": "none",
+            "padding": "0.2em 0.4em",
+            "border_radius": "0.3rem"
         },
     ),
 }
